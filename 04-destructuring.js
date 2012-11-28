592e447d1bbfb09f58e88e167c61029dd6a3201a
aaron frost <aaronfrost@gmail.com>
2012-11-28 14:04:41 -0700

    Updated file names, added two files for Steven.

---
+++ 04-destructuring.js
@@ -0,0 +1,94 @@
+//Swapping values
+
+//Say you have two variables, a and b. Swap them without destructuring:
+
+var a = 1;
+var b = 2;
+
+console.log("a="+a+" b="+b);
+var temp;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log("swapped a="+a+" b="+b);
+
+
+
+//With destructuring, the temp variable is eliminated and the swap takes place in one line of code:
+
+//============================
+//Destructuring - value swap
+var a = 1;
+var b = 2;
+
+console.log("pre swap: a ="+a+", b="+b);
+[a,b] = [b,a];
+
+console.log("post swap: a ="+a+", b="+b);
+
+
+//============================
+
+
+//Destructuring - returning many values:
+
+        var aValue = 1;
+        var bValue = 2;
+        var cValue = 3;
+        var dValue = 4;
+
+     console.log("aValue="+aValue+ ", bValue="+bValue+", cValue="+cValue+ ", dValue="+dValue);
+
+        [aValue,bValue,cValue,dValue] = returnMany(aValue,bValue,cValue,dValue);
+
+     console.log("aValue="+aValue+ ", bValue="+bValue+", cValue="+cValue+ ", dValue="+dValue);
+
+
+    function returnMany(a, b, c, d){
+        return [a+1,b+1,c+1,d+1] ;
+    }
+//============================
+//Destructuring - Matching function parameters
+
+   var array1 = [121,4];
+   var result = destructuring (array1,5);
+
+   console.log(result);
+
+ function destructuring([x,y],z,l,m){
+        console.log("l"+l);
+        return x+y+z+l;
+}
+//============================
+//Destructuring - filtering function parameters
+
+    let result = someElements([2,4,6,8,5]);
+    console.log("result = "+result);
+
+
+
+function someElements([,b,,c])  {
+        console.log("b="+b);
+        console.log("c="+c);
+        return b+c;
+    }
+
+
+// ============================
+// Destructuring - filtering object parameters
+
+        var myObject = {nombre: "Dude",age:"51",gender:"male"};
+        var result = digOutData(myObject);
+
+        console.log("name:"+result.name +" age: "+result.age);
+        
+        
+
+    function digOutData({age:a ,nombre:name, gender:g}){
+        console.log("Name:"+name);   //Name: Dude
+        console.log("Age:"+a);             //Age: 51
+        console.log("Gender:"+g);       //Gender : undefined
+        return {name:name,age:a};
+    }
---
+++ 09-proxies.js
@@ -0,0 +1,5 @@
+/*
+	Nothing supports Proxies yet, so I haven't been able to play with 
+	them so I am mostly unaware of what they do. Sorry, just know
+	that they will be here. 
+*/
